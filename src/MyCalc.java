ffe6aebb2acff9dc00dae60c2f943000109b5d2e
Nitika Maan <nitikamaan15209@gmail.com>
2025-08-26 11:16:06 +0530

    26/Aug/2025

---
+++ src/MyCalc.java
@@ -0,0 +1,8 @@
+public class MyCalc {
+    public static void main(String[] args) {
+        Calculator cl=new Calculator();
+        cl.Area(12.3,10.6);
+        cl.Area(5.7);
+        cl.Area(3.7,8.8,9.4);
+    }
+}
