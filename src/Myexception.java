164e4dd2e0c6174e37bd50bee1c9b14afe68982b
Nitika Maan <nitikamaan15209@gmail.com>
2025-08-29 11:21:53 +0530

    29/08/2025 Exception Handling

--- src/Myexception.java
+++ src/Myexception.java
@@ -1,21 +1,29 @@
+import java.io.DataInputStream;
+import java.io.IOException;
 import java.util.*;
+//import java.util.InputMismatchException
 
 public class Myexception {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         int x, y, z = 0;
-        Scanner sc = new Scanner(System.in);
-        try {
-            x = sc.nextInt();
-            y = sc.nextInt();
-            z = x / y;
-            System.out.println("Exception does not occur");
-        }
+//        Scanner sc = new Scanner(System.in);
+        DataInputStream dis = new DataInputStream(System.in);
+//        try {
+//            x = sc.nextInt();
+//            y = sc.nextInt();
+        x = dis.readInt();
+        y = dis.readInt();
+        z = x / y;
+        System.out.println("Exception does not occur");
+        System.out.println("Result is "+z);
+//        }
 
-        catch(ArithmeticException e){
-                System.out.println(e.getMessage());
-            }
-        finally {
-            System.out.println("Result is " + z);
-        }
+//        catch(ArithmeticException | IOException e){
+//                System.out.println(e.getMessage());
+//            }
+//        finally {
+//            System.out.println("Result is " + z);
+//        }
+//    }
     }
 }
---
+++ src/UserdefinedException.java
@@ -0,0 +1,8 @@
+public class UserdefinedException {
+    public void Validate(int age) throws InvalidAgeException{
+        if(age<18)
+            throw new InvalidAgeException("Cannot cast vote as age is less than 18");
+        else
+            System.out.println("Eligible to vote");
+    }
+}
