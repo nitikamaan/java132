c821f412b98ade7bcb278d947fc461ba250d41c3
Nitika Maan <nitikamaan15209@gmail.com>
2025-08-29 11:49:33 +0530

    29/08/2025 Exception Handling

--- src/MyBank.java
+++ src/MyBank.java
@@ -1,6 +1,11 @@
 public class MyBank {
     public static void main(String[] args) {
         Bank b=new Bank();
-        b.withdraw(5000);
+        try {
+            b.withdraw(12000);
+        }
+        catch(InsufficientBalanceException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
